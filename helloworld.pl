7175085de8f752336f967f7bbbe867219499f5b6
Joshua Eygenraam <83786292+jeygen@users.noreply.github.com>
2023-04-26 22:36:05 -0400

    Create helloworld.pl

---
+++ helloworld.pl
@@ -0,0 +1,23 @@
+#!/usr/bin/perl
+# This line tells the operating system to use the Perl interpreter to execute the script
+
+use strict;
+use warnings;
+# These lines enable strict and warning modes, which enforce stricter rules for variable scoping and declaration,
+# and generate warning messages for common programming mistakes and potential issues in the code
+
+# This is a single-line comment that explains what the following code does
+print "Hello, World!\n";
+# This line prints the message "Hello, World!" to the console, followed by a newline character
+
+=pod
+To download and run the Perl interpreter:
+
+1. Go to the official Perl website at https://www.perl.org/.
+2. Click on the "Downloads" link in the menu at the top of the page.
+3. Choose the appropriate version of Perl for your operating system and follow the instructions to download and install it.
+4. Once Perl is installed, you can run Perl scripts by typing "perl" followed by the name of the script file in a command prompt or terminal window.
+
+sudo apt-get install perl
+
+=cut
