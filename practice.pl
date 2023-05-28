9af508202fcf80df5a42f8dc6b1debd991badc71
debra <debra@debby.myguest.virtualbox.org>
2023-05-28 15:17:22 -0400

    practiced word count

--- practice.pl
+++ practice.pl
@@ -1,4 +1,25 @@
 # Open file and count the total and individual words
 
+my $total_words;
+my %num_per_words;
+
+open(my $wd, '<', 'log_syn1.txt') or die "no good with, $1";
+
+while (my $line = <$wd>) {
+	chomp $line;
+	my @words = split /\s+/, $line;
+	$total_words += scalar @words;
+	foreach my $w (@words) {
+		$num_per_words{$w}++;
+	}
+}
+
+close($wd);
+
+foreach my $key (keys %num_per_words) {
+    print "Key: $key, Value: $hashmap{$key}\n";
+}
+
+print "$total_words";
 
 # Use regex to count occurence of a word
--- wc_counts.pl
+++ wc_counts.pl
@@ -30,3 +30,30 @@ foreach my $word (keys %word_counts) {
 }
 
 print "Total words: $total_words\n";
+
+# my style
+
+my $total_words;
+my %num_per_words;
+
+open(my $wd, '<', 'log_syn1.txt') or die "no good with, $1";
+
+while (my $line = <$wd>) {
+    chomp $line;
+    my @words = split /\s+/, $line;
+    $total_words += scalar @words;
+    foreach my $w (@words) {
+		# [] for char class, [:punct:] special group
+        $w =~ s/[[:punct:]]//g; # This line removes punctuation
+        $num_per_words{$w}++;
+    }
+}
+
+close($wd);
+
+foreach my $key (keys %num_per_words) {
+    print "Key: $key, Value: $num_per_words{$key}\n";  
+}
+
+print "$total_words\n";
+
