29dec7a089ddb58fe8f43e300380105724f17f8e
Joshua Eygenraam <83786292+jeygen@users.noreply.github.com>
2023-06-20 16:30:43 -0400

    Update regex_help.pl

--- regex_help.pl
+++ regex_help.pl
@@ -7,6 +7,9 @@
 #   \b - Matches a word boundary.
 #   \B - Matches a non-word boundary.
 #   . - Matches any character except newline.
+#   m/ is for matching but is optional
+#   /g is all occurences
+#   /i is case insensitive
 
 # Matching a pattern in a string
 if ($string =~ /pattern/) {
