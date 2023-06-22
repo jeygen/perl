214208cdd69b8f032d3396fc6c0a8c3fcf67d010
Joshua Eygenraam <83786292+jeygen@users.noreply.github.com>
2023-06-22 10:42:52 -0400

    Update practice.pl

--- practice.pl
+++ practice.pl
@@ -1,4 +1,15 @@
 # Open file and count the total and individual words
+
+# Use regex to count occurence of a words: cat, catz, dog
+
+# Write a script that reads a file and counts the number of lines, words, and characters.
+# Similar to the Unix 'wc' command.
+
+# Given a server log file, write a script that identifies and prints the most common IP address.
+
+
+
+# Open file and count the total and individual words
  #!/usr/bin/perl
         use strict;
         use warnings;
