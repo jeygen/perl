490eb9dedd965a8dc054558ba543c9b2074be6ec
jeygen <83786292+jeygen@users.noreply.github.com>
2023-10-26 21:55:56 -0400

    new practice q

--- easy_q/pratice.pl
+++ easy_q/pratice.pl
@@ -1,3 +1,15 @@
 # enjoy
 
+# Use regex to count occurence of a words: cat, catz, dog
+# and output to a new file named 'petz.txt'
+
+# Write a script that reads a file and counts the number of lines, words, and characters.
+# Similar to the Unix 'wc' command.
+
+# Given a server log file, write a script that identifies and prints the most common IP address.
+
+# Script to traverse file system and look for files with duplicate content (files could have the
+# same name tho)
+
+# Script to deploy app on multiple servers with diff os, ensure dependecies are all there
 
