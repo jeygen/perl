04c3bf72f3ac4c4c9bd383eca82a204f6c64a493
jeygen <83786292+jeygen@users.noreply.github.com>
2023-10-29 11:51:54 -0400

    add some new file

--- easy_q/pratice.pl
+++ easy_q/pratice.pl
@@ -6,6 +6,10 @@
 # Write a script that reads a file and counts the number of lines, words, and characters.
 # Similar to the Unix 'wc' command.
 
+## this is medium one, research standard packages to know
+# implement a script to find the last modified file in a dir, the path to the dir should be an 
+# arg to the script 
+
 # Given a server log file, write a script that identifies and prints the most common IP address.
 
 # Script to traverse file system and look for files with duplicate content (files could have the
